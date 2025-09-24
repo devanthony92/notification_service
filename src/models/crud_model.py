3218628e2a776fb171be265978795de043731127
Anthony Martinez <aenriquemartinez@gmail.com>
2025-09-23 23:59:24 -0500

    crud completed and functional

--- src/models/crud_model.py
+++ src/models/crud_model.py
@@ -1,7 +1,7 @@
 
-from sqlalchemy import func,Column,Integer,String,Text,DateTime
 from datetime import datetime
-from pydantic import BaseModel, constr 
+from pydantic import BaseModel, constr
+from typing import Optional
 
 
 class Notification(BaseModel):
@@ -14,8 +14,15 @@ class Notification(BaseModel):
     class Config:
         from_attributes = True # Esto permite convertir desde SQLAlchemy a Pydantic
 
-
 class CreateNotification(BaseModel):
     identifying_name: constr(min_length=1,max_length=100) 
     description: constr(min_length=2,max_length=255)
     content_html: constr(min_length=2,max_length=255)
+
+class UpdateNotification(BaseModel):
+    identifying_name: Optional[constr(min_length=1,max_length=100)] = None
+    description: Optional[constr(min_length=1,max_length=255)] = None
+    content_html:  Optional[constr(min_length=1,max_length=255)] = None
+
+    class Config:
+        from_attributes = True # Esto permite convertir desde SQLAlchemy a Pydantic
--- src/services/crud_services.py
+++ src/services/crud_services.py
@@ -1,15 +1,67 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
 from src.models.template_model import Plantillas
-from src.models.crud_model import CreateNotification
+from src.models.crud_model import CreateNotification,UpdateNotification
 
-
-
-def create_notification(db: Session, notification: CreateNotification):
-    db_notification = Plantillas(**notification.dict())
-    db.add(db_notification)
-    db.commit()
-    db.refresh(db_notification)
-    return db_notification
 
 def consult_notifications(db: Session,skip: int = 0, limit: int = 10):
     return db.query(Plantillas).offset(skip).limit(limit).all()
+
+def create_notification(db: Session, notification: CreateNotification):
+    try:
+        db_notification = Plantillas(**notification.dict())
+        db.add(db_notification)
+        db.commit()
+        db.refresh(db_notification)
+        return db_notification
+    except Exception as e: 
+        raise HTTPException(
+            status_code=500,
+            detail="Error interno al crear la notificación"
+        )
+
+def read_notification(id_notification: int, db: Session):
+    try:
+        notification = db.query(Plantillas).filter(Plantillas.id == id_notification).first()
+        if notification is None:
+            raise HTTPException(status_code=404,detail=f"No se encontro notificacion con el ID: {id}, por favor verifique la informacion")
+        return notification
+    except Exception as e: 
+        raise HTTPException(
+            status_code=500,
+            detail="Error interno al consultar la notificación"
+        )
+
+def update_notification(id_notification: int, data: UpdateNotification , db: Session):
+    try:
+        notification = db.query(Plantillas).filter(Plantillas.id == id_notification).first()
+        if notification is None:
+            raise HTTPException(status_code=404,detail=f"No se encontro notificacion con el ID: {id}, por favor verifique la informacion")
+        data_dict = data.dict(exclude_unset=True)
+        for campo, valor in data_dict.items():
+            setattr(notification,campo,valor)
+        db.commit()
+        db.refresh(notification)
+        return notification
+
+    except Exception as e: 
+        raise HTTPException(
+            status_code=500,
+            detail="Error interno al editar la notificación"
+        )
+
+def delete_notification(id_notification: int, db: Session):
+    try:
+        notification = db.query(Plantillas).filter(Plantillas.id == id_notification).first()
+        if notification is None:
+            raise HTTPException(status_code=404,detail=f"No se encontro notificacion con el ID: {id}, por favor verifique la informacion")
+        db.delete(notification)
+        db.commit()
+        return {
+            "message": "Notificacion eliminada correctamente",
+            "status": "success",
+            "data deleted": {notification}
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error interno al eliminar el producto", error = e)
+
